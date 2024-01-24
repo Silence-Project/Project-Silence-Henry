bed9a44b2ca439b8eb6df5f08d7db1b00a0b1acd
KSantiagoRomeroO10@outlook.com <Santiago Romero>
2024-01-24 13:11:59 -0500

    rectificando el proyecto con un commit y push

---
+++ client/src/components/Common/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.f}>Footer</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Common/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <p className={styles.silence}>Silence</p>
+    </header>
+  )
+}
+
+export default Header
