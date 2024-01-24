c88470968e3c46c5648af47d1223af1ae19f6ab0
devpsicoamgg <psico.amgg@gmail.com>
2024-01-24 11:00:56 -0500

    Landing with movement

---
+++ client/src/Components/Commons/Home/Home.jsx
@@ -0,0 +1,13 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className={styles.containerHome}>
+        👋🏽👋🏽👋🏽 Hola, soy el componente llamado Home
+      </div>
+    </>
+  );
+};
+
+export default Home;
