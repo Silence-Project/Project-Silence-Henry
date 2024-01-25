20cb03adbf84c61bbfa5eb60da526049f535cbf1
devpsicoamgg <psico.amgg@gmail.com>
2024-01-24 19:06:03 -0500

    Cambios

--- client/src/Auth/Login/LoginForm.jsx
+++ client/src/Auth/Login/LoginForm.jsx
@@ -1,53 +1,120 @@
-import React, { useState } from "react";
-import SignInForm from "./SignIn";
-import SignUpForm from "./SignUp";
-import styles from './LoginForm.module.css'
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import es from 'date-fns/locale/es';
+import styles from "./LoginForm.module.css";
 
-export default function App() {
-  const [type, setType] = useState("signIn");
-  const handleOnClick = text => {
-    if (text !== type) {
-      setType(text);
-      return;
-    }
+const RegisterForm = () => {
+  const formatDate = (date) => {
+    return date.toISOString().split("T")[0];
   };
-  const containerClass =
-    "container " + (type === "signUp" ? "right-panel-active" : "");
+
+  const initialValues = {
+    fullName: "",
+    lastName: "",
+    birthDate: null,
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    fullName: Yup.string()
+      .matches(/^[A-Za-z]+$/, "El nombre completo no debe contener números")
+      .required("El nombre completo es requerido"),
+    lastName: Yup.string()
+      .matches(/^[A-Za-z]+$/, "Los apellidos no deben contener números")
+      .required("Los apellidos son requeridos"),
+    email: Yup.string()
+      .email("Correo electrónico no válido")
+      .required("El correo electrónico es requerido"),
+    password: Yup.string().required("La contraseña es requerida"),
+  });
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    setSubmitting(false);
+  };
+
   return (
-    <div className={styles.mainContainerForm}>
-      <h2>Sign in/up Form</h2>
-      <div className={containerClass} id="container">
-        <SignUpForm />
-        <SignInForm />
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>
-                To keep connected with us please login with your personal info
-              </p>
-              <button
-                className="ghost"
-                id="signIn"
-                onClick={() => handleOnClick("signIn")}
-              >
-                Sign In
-              </button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button
-                className="ghost "
-                id="signUp"
-                onClick={() => handleOnClick("signUp")}
-              >
-                Sign Up
-              </button>
-            </div>
+    <div className={styles.form}>
+      <h2>Registro</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="fullName">Nombres:</label>
+            <Field type="text" id="fullName" name="fullName" />
+            <ErrorMessage
+              name="fullName"
+              component="div"
+              className={styles.error}
+            />
           </div>
-        </div>
-      </div>
+
+          <div>
+            <label htmlFor="lastName">Apellidos:</label>
+            <Field type="text" id="lastName" name="lastName" />
+            <ErrorMessage
+              name="lastName"
+              component="div"
+              className={styles.error}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="birthDate">Fecha de Nacimiento:</label>
+            <Field name="birthDate">
+              {({ field, form }) => (
+                <DatePicker
+                  id="birthDate"
+                  selected={field.value}
+                  onChange={(date) => form.setFieldValue("birthDate", date)}
+                  onBlur={() => form.setFieldTouched("birthDate", true)}
+                  peekNextMonth
+                  showMonthDropdown
+                  showYearDropdown
+                  dropdownMode="select"
+                  placeholderText="Selecciona fecha"
+                   locale={es} 
+                />
+              )}
+            </Field>
+            <ErrorMessage
+              name="birthDate"
+              component="div"
+              className={styles.error}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email">Correo electrónico:</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className={styles.error}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password">Contraseña:</label>
+            <Field type="password" id="password" name="password" />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className={styles.error}
+            />
+          </div>
+
+          <button type="submit">Registrarse</button>
+        </Form>
+      </Formik>
     </div>
   );
-}
+};
+
+export default RegisterForm;
