f3917f15891397343521312b24280b12fb52ba0e
odnxmrx <armandopinedag@outlook.com>
2024-01-27 17:24:07 -0700

    create controller for user by id and email

--- server/src/handlers/userHandler.js
+++ server/src/handlers/userHandler.js
@@ -18,21 +18,30 @@ userHandler.post("/", async (req, res) => {
   }
 });
 
-
-//GET by param email
+//GET by param email - /email@mail.com
 /**
- * Necesidad de verificar/validar que no exista una cuenta ya 
+ * Necesidad de verificar/validar que no exista una cuenta ya
  * con el correo proporcionado a través del formulario de 'Crear cuenta'
  * Retorna {
- *  idUser ? Int 
+ *  idUser ? Int
  *  email exists ? boolean
  *  isActive ? boolean
  * }
-*/
+ */
 userHandler.get("/", async (req, res) => {
   const { email } = req.query;
 
   try {
+    if (!email) {
+      const usuarios = await Usuario.findAll({
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      });
+      //return para cortar bloque
+      return res.status(200).json(usuarios);
+    }
+
     let userByEmail = await getUserByEmail(email);
     res.status(200).json(userByEmail);
   } catch (error) {
@@ -40,21 +49,35 @@ userHandler.get("/", async (req, res) => {
   }
 });
 
-
-//GET all
-userHandler.get("/", async (req, res) => {
+//GET by user ID - /:id params
+userHandler.get("/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const usuarios = await Usuario.findAll({
-      attributes: {
-        exclude: ["createdAt", "updatedAt"],
-      },
-    });
+    const singleUser = await Usuario.findByPk(id);
 
-    res.status(200).json(usuarios);
+    if (!singleUser)
+      return response.status(404).send(`Usuario con código ${code} no existe.`);
+
+    return res.status(200).json(singleUser);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
+
+//GET all
+// userHandler.get("/", async (req, res) => {
+//   try {
+//     const usuarios = await Usuario.findAll({
+//       attributes: {
+//         exclude: ["createdAt", "updatedAt"],
+//       },
+//     });
+
+//     res.status(200).json(usuarios);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
 module.exports = {
   userHandler,
