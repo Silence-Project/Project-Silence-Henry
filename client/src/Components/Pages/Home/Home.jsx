36741d5bbf1824d410689560b43719a27f08c732
odnxmrx <armandopinedag@outlook.com>
2024-01-31 09:55:53 -0700

    adjust css flexbox holy grail layout

--- client/src/Components/Pages/Home/Home.jsx
+++ client/src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from "react";
 import styles from "./Home.module.css";
-import Cards from "../../Common/ProductList/ProductList";
+import ProductList from "../../Common/ProductList/ProductList";
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from "../../../Redux/Store/Slices/ProductSlice";
 import { Link } from "react-router-dom";
 import ROUTES from "../../../Helpers/Routes.helper";
-
-
 
 import Header from '../../Common/Header/Header';
 import Footer from '../../Common/Footer/Footer';
@@ -19,38 +17,33 @@ const Home = () => {
     dispatch(getProducts());
   }, [dispatch]);
 
- 
-
-
   const products = useSelector((state) => state.product.products);
 
   console.log("PRODUCTOS ->", products);
 
-  const estilosCss = 'Home'
+  // const estilosCss = 'Home'
 
   return (
-    <>
-      <div className={styles.homeContainer}>
+    <div className={styles.homeContainer}>
 
-        <Descuento/>
+      <Descuento />
 
-        <Header estilosCss={estilosCss}/>
+      <Header />
 
-        <div className={styles.navbar}>
-          <Link to={ROUTES.CREATE_PRODUCT}>Crear Producto</Link>
-        </div>
-
-        <div className={styles.cardContainer}>
-              <Cards className='card' products={products}/>
-        </div>
-
-        <div className={styles.navbar}>
-          <Link to={ROUTES.LOGGING}>Inicia Sesión</Link>
-        </div>
-
-        <Footer/>
+      <div className={styles.navbar}>
+        <Link to={ROUTES.CREATE_PRODUCT}>Crear Producto</Link>
       </div>
-    </>
+
+      {/* <div className={styles.cardContainer}> */}
+        <ProductList className='card' products={products} />
+      {/* </div> */}
+
+      {/* <div className={styles.navbar}>
+        <Link to={ROUTES.LOGGING}>Inicia Sesión</Link>
+      </div> */}
+
+      <Footer />
+    </div>
   );
 };
 
