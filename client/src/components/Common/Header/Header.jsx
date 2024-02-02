76034a43a0dcd6cdd0533505a155e83d1ece8551
devpsicoamgg <psico.amgg@gmail.com>
2024-02-02 12:15:02 -0500

    returning name

--- client/src/components/Common/Header/Header.jsx
+++ client/src/components/Common/Header/Header.jsx
@@ -9,7 +9,7 @@ import MenuHamburger from '../MenuHamburger/MenuHamburger'
 // import mailInboxIcon from '../../../img/icons/mail-inbox-app.png'
 
 
-const Head = () => {
+const Header = () => {
 
   return (
     <div className={styles.header1}>
@@ -29,4 +29,4 @@ const Head = () => {
   )
 }
 
-export default Head
+export default Header
