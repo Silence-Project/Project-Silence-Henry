923024e0d8029c1d39c5d071ab2e03d89e786276
devpsicoamgg <psico.amgg@gmail.com>
2024-02-02 11:55:18 -0500

    Ordering the home

--- client/src/Components/Pages/Home/Home.jsx
+++ client/src/Components/Pages/Home/Home.jsx
@@ -1,43 +1,35 @@
 // import { Link } from "react-router-dom";
 // import ROUTES from "../../../Helpers/Routes.helper";
 
-import { useEffect } from "react";
-import styles from "./Home.module.css";
-import Cards from '../../Common/ProductList/ProductList'
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../../Redux/Store/Slices/ProductSlice";
-import Header from "../../Common/Header/Header";
-import Footer from '../../Common/Footer/Footer';
-import Descuento from '../../Common/Descuento/Descuento'
+import Header from "../../../Components/Common/Header/Header.jsx";
+import Cards from "../../Common/ProductList/ProductList";
+import Footer from "../../Common/Footer/Footer";
+import Descuento from "../../Common/Descuento/Descuento";
+import styles from "./Home.module.css";
 
 const Home = () => {
   const dispatch = useDispatch();
-
-const products = useSelector((state) => state.product.products);
-
+  const products = useSelector((state) => state.product.products);
 
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
 
-
   console.log("PRODUCTOS ->", products);
 
   return (
-    <>
-      <div className={styles.homeContainer}>
-        <Descuento />
-
-        <Header />
-        
-        <div className={styles.cardContainer}>
-          <Cards className='card' products={products}/>
-        </div>    
-
-        <Footer/>
+    <div className={styles.homeContainer}>
+      <Descuento />
+      <Header />
+      <div className={styles.cardContainer}>
+        <Cards className="card" products={products} />
       </div>
-    </>
-  )
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
