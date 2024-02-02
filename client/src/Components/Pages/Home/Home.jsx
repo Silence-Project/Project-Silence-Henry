4b166e765ccf58a5079324748041cc44f89a79b5
devpsicoamgg <psico.amgg@gmail.com>
2024-02-02 11:34:29 -0500

    Order in home

--- client/src/Components/Pages/Home/Home.jsx
+++ client/src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
+// import { Link } from "react-router-dom";
+// import ROUTES from "../../../Helpers/Routes.helper";
+
 import { useEffect } from "react";
 import styles from "./Home.module.css";
 import Cards from '../../Common/ProductList/ProductList'
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from "../../../Redux/Store/Slices/ProductSlice";
-// import { Link } from "react-router-dom";
-// import ROUTES from "../../../Helpers/Routes.helper";
-
 import Header from '../../Common/Header/Header';
 import Footer from '../../Common/Footer/Footer';
 import Descuento from '../../Common/Descuento/Descuento'
