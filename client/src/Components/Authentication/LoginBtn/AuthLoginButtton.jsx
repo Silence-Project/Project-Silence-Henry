f6f307b0530da169095f0882c590b3ea419c4f29
odnxmrx <armandopinedag@outlook.com>
2024-02-03 15:59:28 -0700

    implement 3rd party auth and restricted views

--- client/src/Components/Authentication/LoginBtn/AuthLoginButtton.jsx
+++ client/src/Components/Authentication/LoginBtn/AuthLoginButtton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+const AuthLoginButtton = () => {
   const { loginWithRedirect } = useAuth0();
 
   return <button onClick={() => loginWithRedirect()}>Log In Auth0</button>;
 };
 
-export default LoginButton;
+export default AuthLoginButtton;
+//https://i.postimg.cc/9z5FwpW7/silence-logo.jpg
