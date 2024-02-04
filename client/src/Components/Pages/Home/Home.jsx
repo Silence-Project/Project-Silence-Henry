f47b1bb7a1ef4ee09b6d1d033838be74aa22ec66
valcar.cortes@gmail.com <ValcarC>
2024-02-04 13:08:29 -0600

    filters

--- client/src/Components/Pages/Home/Home.jsx
+++ client/src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,6 @@
 // import { Link } from "react-router-dom";
 // import ROUTES from "../../../Helpers/Routes.helper";
 
-<<<<<<< HEAD
-import Header from '../../Common/Header/Header';
-import Footer from '../../Common/Footer/Footer';
-import Descuento from '../../Common/Descuento/Descuento'
-import Sidebar from "../../Common/Sidebar/Sidebar";
-=======
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../../Redux/Store/Slices/ProductSlice";
@@ -14,8 +8,10 @@ import Head from "../../Common/Header/Head";
 import Cards from "../../Common/ProductList/ProductList";
 import Footer from "../../Common/FooterView/Footer";
 import Descuento from "../../Common/Descuento/Descuento";
+import Sidebar from "../../Common/Sidebar/Sidebar"
 import styles from "./Home.module.css";
->>>>>>> 48dfbdee2f5190d5338d762fc278ccb6efb1efd4
+
+import Filter from "../../Common/Filters/Filters";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -28,29 +24,15 @@ const Home = () => {
   console.log("PRODUCTOS ->", products);
 
   return (
-<<<<<<< HEAD
-    <>
-      <div className={styles.homeContainer}>
-        <Descuento />
-
-        <Header />
-
-        <Sidebar />
-        
-        <div className={styles.cardContainer}>
-          <Cards className='card' products={products}/>
-        </div>    
-
-        <Footer/>
-=======
     <div className={styles.homeContainer}>
       <Descuento />
       <Head />
       <div className={styles.cardContainer}>
         <Cards className="card" products={products} />
->>>>>>> 48dfbdee2f5190d5338d762fc278ccb6efb1efd4
       </div>
       <Footer />
+      <Sidebar />
+      <Filter />
     </div>
   );
 };
