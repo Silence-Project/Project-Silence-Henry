a635696af5d09e421f2e83d1b8a8936696c0a324
odnxmrx <armandopinedag@outlook.com>
2024-02-03 19:39:10 -0700

    adding optional redirecturi helper

--- client/src/Helpers/auth0ProviderWithNavigate.jsx
+++ client/src/Helpers/auth0ProviderWithNavigate.jsx
@@ -15,7 +15,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
   //access environment variables
   const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_REACT_APP_AUTH0_CALLBACK_URL;
+  const redirectUri = "https://silencepage.netlify.app/home" || import.meta.env.VITE_REACT_APP_AUTH0_CALLBACK_URL;
   
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
