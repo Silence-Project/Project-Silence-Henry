b19b5f54f1ebe0104105d592d7523962bebfeae2
Santiago Romero <KSantiagoRomeroO10@outlook.com>
2024-02-04 21:29:59 -0500

    Arreglando el front

--- client/src/Components/Pages/Home/Home.jsx
+++ client/src/Components/Pages/Home/Home.jsx
@@ -1,21 +1,15 @@
 // import { Link } from "react-router-dom";
 // import ROUTES from "../../../Helpers/Routes.helper";
 
-<<<<<<< HEAD
-import Header from '../../Common/Header/Header';
-import Footer from '../../Common/Footer/Footer';
 import Descuento from '../../Common/Descuento/Descuento'
 import Sidebar from "../../Common/Sidebar/Sidebar";
-=======
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../../Redux/Store/Slices/ProductSlice";
 import Head from "../../Common/Header/Head";
 import Cards from "../../Common/ProductList/ProductList";
 import Footer from "../../Common/FooterView/Footer";
-import Descuento from "../../Common/Descuento/Descuento";
 import styles from "./Home.module.css";
->>>>>>> 48dfbdee2f5190d5338d762fc278ccb6efb1efd4
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -28,13 +22,11 @@ const Home = () => {
   console.log("PRODUCTOS ->", products);
 
   return (
-<<<<<<< HEAD
     <>
       <div className={styles.homeContainer}>
         <Descuento />
 
-        <Header />
-
+        <Head />
         <Sidebar />
         
         <div className={styles.cardContainer}>
@@ -42,16 +34,8 @@ const Home = () => {
         </div>    
 
         <Footer/>
-=======
-    <div className={styles.homeContainer}>
-      <Descuento />
-      <Head />
-      <div className={styles.cardContainer}>
-        <Cards className="card" products={products} />
->>>>>>> 48dfbdee2f5190d5338d762fc278ccb6efb1efd4
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
