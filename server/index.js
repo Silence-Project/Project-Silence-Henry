96881047f6b97960d7fc1bd8215ac578cdf4f530
devpsicoamgg <psico.amgg@gmail.com>
2024-02-12 19:00:14 -0500

    Fizing a big mistake mine

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/server');
-// const PORT = 5432;
-const PORT = 3001;
+ const PORT = 5432;
+//const PORT = 3001;
 const { conn } = require('./src/config/bd');
 
 conn.sync({ force: false })
