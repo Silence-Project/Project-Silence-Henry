e73cad4176c3813fe54d9f2f2ce4e90a6feb833f
Santiago Romero <KSantiagoRomeroO10@outlook.com>
2024-02-13 20:09:57 -0500

    Carrito arreglado

--- client/src/Components/Common/Carrito/Carrito.jsx
+++ client/src/Components/Common/Carrito/Carrito.jsx
@@ -5,6 +5,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import ROUTES from '../../../Helpers/Routes.helper';
 
+import Head from "../../Common/Header/Head";
+
+import Footer from "../../Common/FooterView/Footer";
+
 import styles from './Carrito.module.css'
 
 import { getCarrito, sincronizarDB, deleteProductDb } from "../../../Redux/Store/Slices/CarritoSlice";
@@ -73,53 +77,57 @@ const CarritoSlides = () => {
   const idProducts = productos.map(element => element.id);
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.h2}>Carrito de Compras</h2>
-      <br />
-      <table className={styles.table}>
-        <tr>
-          <th>Nombre</th>
-          <th>Cantidad</th>
-          <th>Precio Unitario</th>
-          <th>Precio Total</th>
-          <th></th>
-        </tr>
-        {productos.map(producto => (  
-          <>
-            <tr key={producto.id}>
-              <td className={styles.name}>{producto.name}</td>
-              <td className={styles.cantidad}>{producto.cantidad}</td>
-              <td className={styles.price}>{producto.price}</td>
-              <td className={styles.totalUnitario}>{totales.push(producto.price * producto.cantidad) && producto.price * producto.cantidad}</td>
-              <td className={styles.button}> 
-                {/* <button onClick={handlerDrop(producto.id)}>Eliminar</button>  */}
-                <button onClick={() => handlerDrop(producto.id)}>Eliminar</button>
-              </td>
-            </tr>
-          </>                
-
-        ))}
-          <tr className={styles.lastTotal}>
-            <td></td>
-            <td></td>
-            <td className={styles.totalComprasText}>Total de compra:</td>
-            <td className={styles.totalCompras}>{totales ? totales.reduce((acumulador, valorActual) => acumulador + valorActual, 0) : 0}</td>
+    <>
+      <Head/>
+      <div className={styles.container}>
+        <h2 className={styles.h2}>Carrito de Compras</h2>
+        <br />
+        <table className={styles.table}>
+          <tr>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Precio Unitario</th>
+            <th>Precio Total</th>
+            <th></th>
           </tr>
-      </table>
+          {productos.map(producto => (  
+            <>
+              <tr key={producto.id}>
+                <td className={styles.name}>{producto.name}</td>
+                <td className={styles.cantidad}>{producto.cantidad}</td>
+                <td className={styles.price}>{producto.price}</td>
+                <td className={styles.totalUnitario}>{totales.push(producto.price * producto.cantidad) && producto.price * producto.cantidad}</td>
+                <td className={styles.button}> 
+                  {/* <button onClick={handlerDrop(producto.id)}>Eliminar</button>  */}
+                  <button onClick={() => handlerDrop(producto.id)}>Eliminar</button>
+                </td>
+              </tr>
+            </>                
 
-      <button onClick={() => handlerBorrarTodo(idProducts)}>Borrar todo</button>
+          ))}
+            <tr className={styles.lastTotal}>
+              <td></td>
+              <td></td>
+              <td className={styles.totalComprasText}>Total de compra:</td>
+              <td className={styles.totalCompras}>{totales ? totales.reduce((acumulador, valorActual) => acumulador + valorActual, 0) : 0}</td>
+            </tr>
+        </table>
 
-      <Link to="/checkout" >
-        <button>Comprar</button>
+        <button onClick={() => handlerBorrarTodo(idProducts)}>Borrar todo</button>
+
+        <Link to="/checkout" >
+          <button>Comprar</button>
+        </Link>
+      
+      <br></br>
+
+      <Link to={ROUTES.HOME}>
+        <button>Home</button>
       </Link>
-    
-    <br></br>
 
-    <Link to={ROUTES.HOME}>
-      <button>Home</button>
-    </Link>
-
-    </div>
+      </div>
+      <Footer/>
+    </>
 
   );
 };
