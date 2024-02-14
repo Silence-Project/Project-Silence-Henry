41b35150a86f0abec7b86da1c0d4856c1f8cc800
devpsicoamgg <psico.amgg@gmail.com>
2024-02-14 15:14:36 -0500

    Fixing minor bugs

--- client/src/Helpers/routesToChange.js
+++ client/src/Helpers/routesToChange.js
@@ -1,6 +1,6 @@
 const URLTOCHANGE = {
- theUrl: "http://127.0.0.1:3001",
-// theUrl: "https://silenceback.onrender.com",
+// theUrl: "http://127.0.0.1:3001",
+ theUrl: "https://silenceback.onrender.com",
 };
 
 export default URLTOCHANGE;
