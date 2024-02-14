6f2f8d7898cf30c6615c2e893cec81c05e91cad0
odnxmrx <armandopinedag@outlook.com>
2024-02-14 12:43:52 -0700

    add teporal changes to profile

--- client/src/Components/UserOrders/UserOrders.jsx
+++ client/src/Components/UserOrders/UserOrders.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import URLTOCHANGE from "../../Helpers/routesToChange";
+import style from "./UserOrders.module.css";
 
 const UserOrders = ({currentUser}) => {
 
@@ -11,10 +12,44 @@ const UserOrders = ({currentUser}) => {
     console.log('que es response?', response);
   }
 
+  const simulaOrders = [
+    {
+      id: 1,
+      statePayment: "completado",
+      ShippingMethod: "correos argentina",
+      createdAt: "2024-02-02 00:00:00",
+      updatedAt: "2024-02-02 00:00:00",
+      idUserOrder: 2
+    },
+    {
+      id: 2,
+      statePayment: "completado",
+      ShippingMethod: "correos argentina",
+      createdAt: "2024-02-03 00:00:00",
+      updatedAt: "2024-02-03 00:00:00",
+      idUserOrder: 2
+    },
+    {
+      id: 7,
+      statePayment: "cancelado",
+      ShippingMethod: "correos argentina",
+      createdAt: "2024-02-02 00:00:00",
+      updatedAt: "2024-02-02 00:00:00",
+      idUserOrder: 2
+    }
+  ]
 
   return(
-    <div>
-      {/* hola */}
+    <div className={style.fullOrderContainer}>
+      {
+        simulaOrders.map(order => (
+          <div key={order.id} className={style.orderContainer}>
+            <p>Fecha: {order.createdAt}</p>
+            <p>Status: {order.statePayment}</p>
+            <p>Método de entrega: {order.ShippingMethod}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
