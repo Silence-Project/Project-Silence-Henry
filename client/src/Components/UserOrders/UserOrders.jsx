f3cecd0e5a76ab0c5839209b1c37a48a6751b047
odnxmrx <armandopinedag@outlook.com>
2024-02-14 14:02:54 -0700

    add user orders tab

--- client/src/Components/UserOrders/UserOrders.jsx
+++ client/src/Components/UserOrders/UserOrders.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import URLTOCHANGE from "../../Helpers/routesToChange";
 import style from "./UserOrders.module.css";
@@ -7,42 +7,29 @@ const UserOrders = ({currentUser}) => {
 
   const { id } = currentUser;
 
-  async function consultOrders() {
-    const response = await axios(`${URLTOCHANGE.theUrl}/orders/${id}`);
-    console.log('que es response?', response);
-  }
+  const [myOrders, setMyOrders] = useState([]);
 
-  const simulaOrders = [
-    {
-      id: 1,
-      statePayment: "completado",
-      ShippingMethod: "correos argentina",
-      createdAt: "2024-02-02 00:00:00",
-      updatedAt: "2024-02-02 00:00:00",
-      idUserOrder: 2
-    },
-    {
-      id: 2,
-      statePayment: "completado",
-      ShippingMethod: "correos argentina",
-      createdAt: "2024-02-03 00:00:00",
-      updatedAt: "2024-02-03 00:00:00",
-      idUserOrder: 2
-    },
-    {
-      id: 7,
-      statePayment: "cancelado",
-      ShippingMethod: "correos argentina",
-      createdAt: "2024-02-02 00:00:00",
-      updatedAt: "2024-02-02 00:00:00",
-      idUserOrder: 2
+  useEffect(()=> {
+
+    const consultOrders = async () => {
+      try {
+        const response = await axios(`${URLTOCHANGE.theUrl}/orders/${id}`);
+        // console.log('que es response?', response);
+        const data = response.data;
+        setMyOrders(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
     }
-  ]
+    consultOrders();
+  }, [])
+
 
   return(
     <div className={style.fullOrderContainer}>
       {
-        simulaOrders.map(order => (
+        myOrders && myOrders.map(order => (
           <div key={order.id} className={style.orderContainer}>
             <p>Fecha: {order.createdAt}</p>
             <p>Status: {order.statePayment}</p>
