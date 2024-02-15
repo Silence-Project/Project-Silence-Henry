630e57f2ee13b67827150096ea77e4d2aecd3d62
Luis Antonio Giaccio <luisantoniogiaccio@gmail.com>
2024-02-15 05:04:15 -0300

    VERIFICACIONES ADMIN

--- client/src/Components/Common/Header/Head.jsx
+++ client/src/Components/Common/Header/Head.jsx
@@ -9,6 +9,7 @@ import userIcon from '../../../img/icons/user-icon.png';
 import { useAuth0 } from "@auth0/auth0-react";
 import SearchBar from "../Sidebar/Search/SearchBar";
 import Descuento from "../Descuento/Descuento";
+import { useSelector } from "react-redux";
 
 const Head = ({
   setFilterTerm,
@@ -20,49 +21,99 @@ const Head = ({
 }) => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
 
-  return (
-    <div>
-      <Descuento className={styles.descuento} />
-      <div className={styles.header1}>
-        <MenuHamburger
-          handleColorChange={handleColorChange}
-          handleSizeChange={handleSizeChange}
-          handlePriceChange={handlePriceChange}
-          handleCategoryChange={handleCategoryChange}
-          handleMaterialChange={handleMaterialChange}
-        />
-        <SearchBar setFilterTerm={setFilterTerm} />
-        <NavLink to={ROUTES.HOME}>
-          <img src={imgLogo} alt="silence" className={styles.silence} />
-        </NavLink>
-        {isAuthenticated && (
-          <NavLink to="/profile">
-            <img src={userIcon} alt="user icon" className={styles.userIcon} />
-          </NavLink>
-        )}
-        {!isAuthenticated && (
-          <span onClick={() => loginWithRedirect()}>
-            <img src={userIcon} alt="user icon" className={styles.userIcon} />
-          </span>
-        )}
-        <NavLink to={ROUTES.Carrito}>
-          <img
-            src={shoppingCartIcon}
-            alt="kart market"
-            className={styles.kartMarket}
+
+  const currentUser = useSelector((state) => state.user.user)
+
+  if (currentUser && currentUser.isAdmin === true) {
+    return (
+      <div>
+        <Descuento className={styles.descuento} />
+        <div className={styles.header1}>
+          <MenuHamburger
+            handleColorChange={handleColorChange}
+            handleSizeChange={handleSizeChange}
+            handlePriceChange={handlePriceChange}
+            handleCategoryChange={handleCategoryChange}
+            handleMaterialChange={handleMaterialChange}
           />
-        </NavLink>
+          <SearchBar setFilterTerm={setFilterTerm} />
+          <NavLink to={ROUTES.HOME}>
+            <img src={imgLogo} alt="silence" className={styles.silence} />
+          </NavLink>
+          {isAuthenticated && (
+            <NavLink to="/profile">
+              <img src={userIcon} alt="user icon" className={styles.userIcon} />
+            </NavLink>
+          )}
+          {!isAuthenticated && (
+            <span onClick={() => loginWithRedirect()}>
+              <img src={userIcon} alt="user icon" className={styles.userIcon} />
+            </span>
+          )}
+          <NavLink to={ROUTES.Carrito}>
+            <img
+              src={shoppingCartIcon}
+              alt="kart market"
+              className={styles.kartMarket}
+            />
+          </NavLink>
+  
+          <NavLink to= "/favoritos">
+            <button className={styles.favoritos}>Favoritos</button>
+          </NavLink>
 
-        <NavLink to= "/favoritos">
-          <button className={styles.favoritos}>Favoritos</button>
-        </NavLink>
-
-        <NavLink to= "/suspension">
-          <button className={styles.suspension}>Suspension</button>
-        </NavLink>
+          <NavLink to= "/suspension">
+            <button className={styles.suspension}>Suspension</button>
+          </NavLink>
+  
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <Descuento className={styles.descuento} />
+        <div className={styles.header1}>
+          <MenuHamburger
+            handleColorChange={handleColorChange}
+            handleSizeChange={handleSizeChange}
+            handlePriceChange={handlePriceChange}
+            handleCategoryChange={handleCategoryChange}
+            handleMaterialChange={handleMaterialChange}
+          />
+          <SearchBar setFilterTerm={setFilterTerm} />
+          <NavLink to={ROUTES.HOME}>
+            <img src={imgLogo} alt="silence" className={styles.silence} />
+          </NavLink>
+          {isAuthenticated && (
+            <NavLink to="/profile">
+              <img src={userIcon} alt="user icon" className={styles.userIcon} />
+            </NavLink>
+          )}
+          {!isAuthenticated && (
+            <span onClick={() => loginWithRedirect()}>
+              <img src={userIcon} alt="user icon" className={styles.userIcon} />
+            </span>
+          )}
+          <NavLink to={ROUTES.Carrito}>
+            <img
+              src={shoppingCartIcon}
+              alt="kart market"
+              className={styles.kartMarket}
+            />
+          </NavLink>
+  
+          <NavLink to= "/favoritos">
+            <button className={styles.favoritos}>Favoritos</button>
+          </NavLink>
+  
+         
+        </div>
+      </div>
+    );
+  }
+
+
 };
 
 export default Head;
